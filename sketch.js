83233e673747a66324c72f9c858127487d5dc151
shreyansh110 <anuj.goel1999@gmail.com>
2020-09-20 11:54:12 +0530

    I have created a pig to the project

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,pig2;
 var backgroundImg,platform;
 
 function preload() {
@@ -22,6 +22,7 @@ function setup(){
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
+    pig2 =new Pig(200,200);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
@@ -58,6 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    pig2.display();
 
     bird.display();
     platform.display();
